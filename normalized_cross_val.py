00e3e2182ef8f435eb11477d66092d1eae8bac2b
hesam-a <40280976+hesam-a@users.noreply.github.com>
2021-10-26 11:00:30 -0400

    Update normalized_cross_val.py
    latest change to NN model

--- normalized_cross_val.py
+++ normalized_cross_val.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-np.set_printoptions(precision=3, suppress=True)
+np.set_printoptions(precision=10, suppress=True)
 
 import tensorflow as tf
 from tensorflow.keras import layers
@@ -20,6 +20,12 @@ def miss_val_fixer(data):
     fixed_data = imp.transform(data)
     return fixed_data
 
+def standardize(train,valid):
+    scaler = StandardScaler()
+    std_train = scaler.fit_transform(train)
+    std_valid = scaler.transform(valid)
+    return std_train,std_valid
+
 def normalizer(data):
     layer = preprocessing.Normalization(axis=None)
     layer.adapt(data)
@@ -27,40 +33,30 @@ def normalizer(data):
     array_data = np.array(normalized_data) 
     return array_data
 
-def baseline_model():
+def nn_model():
     model = Sequential()
-#    model.add(Dense(500, input_dim=1576, kernel_initializer='normal', activation='relu'))
-#    model.add(Dense(100, input_dim=1576, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(50, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(10, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(1 , kernel_initializer='normal'))
-    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.add(Dense(500, activation='relu', kernel_regularizer=l2(0.0001),input_shape=train_X.shape[1:])
+    model.add(Dense(200, kernel_regularizer=l2(0.0001), activation='relu')) # kernel_initializer='he_normal',
+    model.add(Dense(100, kernel_regularizer=l2(0.0001), activation='relu')) # kernel_initializer='he_normal',
+    model.add(Dense(50 , kernel_regularizer=l2(0.0001), activation='relu')) # kernel_initializer='he_normal'
+    model.add(Dense(20, kernel_regularizer=l2(0.0001), activation='relu')) # kernel_initializer='he_normal',
+    model.add(Dense(1 )# kernel_initializer='he_normal'))
+    opt = tf.keras.optimizers.Adam(learning_rate=0.0001)
+    model.compile(loss='mean_squared_error', optimizer='opt',metrics=['mae'])
     return model
 
-train_set = pd.read_csv("SMRT_fixed.csv", nrows=60000,low_memory=False)
-train_set = miss_val_fixer(train_set)
-train_set = train_set[:,2:]
-norm_set = normalizer(train_set) 
+# dataset
+dataset = pd.read_csv("SMRT_fixed.csv",low_memory=False)
+dataset = miss_val_fixer(dataset)
+data = dataset[:,2:]
+train_set, test_set = train_test_split(data, test_size=0.25, random_state=42)
 
-train_X = norm_set[:,:-1]
-train_Y = norm_set[:,-1:]
+train_X, test_X = standardize(train_set[:,:-1],test_set[:,:-1])
+train_Y = train_set[:,-1]/10
+test_Y = test_set[:,-1]/10
 
-# normalizing training X and Y
-#norm_X = normalizer(train_X)
-#norm_Y = normalizer(train_Y)
-
-estimator = KerasRegressor(build_fn=baseline_model, epochs=100, verbose=0)
-kfold = KFold(n_splits=40)
+# NN model
+estimator = KerasRegressor(build_fn=nn_model, epochs=20, verbose=0)
+kfold = KFold(n_splits=20)
 results = cross_val_score(estimator,train_X, train_Y, cv=kfold)
 print("mean: %.2f (%.2f) std dev" % (results.mean(), results.std()))
-
-#prediction = estimator.predict(X_test)
-
-
-#estimators = []
-#estimators.append(('standardize', StandardScaler()))
-#estimators.append(('mlp', KerasRegressor(build_fn=baseline_model, epochs=50, verbose=0)))
-#pipeline = Pipeline(estimators)
-#kfold = KFold(n_splits=10)
-#results = cross_val_score(pipeline, train_X, train_Y, cv=kfold)
-#print("Baseline: %.2f (%.2f) MSE" % (results.mean(), results.std()))
